200a92ca9c621f1e1c5b61b2bd2464677733883a
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-20 19:38:27 +0100

    Add swagger-io and openai.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from "cors";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import OpenAI from "openai";
 
+//Routes
+import v1Routes from './routes/v1';
+
+
+//Configutations
+dotenv.config();
 const app = express();
-
-app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('🎉 LangPulse API is running!');
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+//Middlewares
+app.use(express.json())
+app.use(cors())
+
+//Routes
+app.use('/api/v1', v1Routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
