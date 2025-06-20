a69239d6830f39ab297f4f7197488aaa2c6d327d
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-20 19:36:10 +0100

    Update sever file.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
+
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
