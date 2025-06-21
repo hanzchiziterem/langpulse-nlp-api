44eb221eb207d54ae2e1fac2433b46a36dc87562
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-21 20:33:08 +0100

    Add analysis history route.

--- src/routes/v1/nlp.route.ts
+++ src/routes/v1/nlp.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { analyzeTextHandler } from "../../controllers/nlp.controller";
+import { analyzeTextHandler, getAnalysisHistory } from "../../controllers/nlp.controller";
 import { requireAuth } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/analyze", requireAuth, analyzeTextHandler);
-
+router.get("/history", requireAuth, getAnalysisHistory);
 export default router;
