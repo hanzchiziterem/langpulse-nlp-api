a9cfa0a1d67859d2722f1c4fb8fdac3680410e65
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-21 20:34:43 +0100

    Implemet db to store analysis.

--- src/services/nlp.service.ts
+++ src/services/nlp.service.ts
@@ -1,7 +1,7 @@
 import { openai } from "../app";
+import prisma from "../lib/prisma";
 
-//Add this later as param when implementing psql, userId: number,
-export const analyzeText = async (text: string) => {
+export const analyzeText = async (userId:string, text: string) => {
   try {
     const completion = await openai.chat.completions.create({
       model: "gpt-4o-mini",
@@ -23,9 +23,8 @@ export const analyzeText = async (text: string) => {
 
     const result =
       completion.choices[0].message?.content || "No result returned";
-
-    // Save to DB like before (i will use Prisma)
-    // await prisma.analysis.create({ data: { userId, text, result } });
+    
+    await prisma.analysis.create({ data: { userId, text, result } });
     
     return JSON.parse(result);
   } catch (error: any) {
