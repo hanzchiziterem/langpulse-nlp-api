7bc9d7763de0d1fa7fc92be40ddbccad0fe56569
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-21 20:35:53 +0100

    Implemet db to user data.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,29 +1,26 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { users, User } from "../models/user.model";
+import prisma from "../lib/prisma";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-//Use Arrays implement Prisma and PostgreSQL later.
-//Test with postman after.
-
-export const signupUser = async (name: string, email: string, password: string) => {
-  const existing = users.find((user) => user.email === email);
+export const signupUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const existing = await prisma.user.findUnique({ where: { email } });
   if (existing) throw new Error("User already exists");
 
   const hashed = await bcrypt.hash(password, 10);
-  const newUser: User = {
-    id: Date.now(),
-    name,
-    email,
-    password: hashed,
-  };
-  users.push(newUser);
+  const newUser = await prisma.user.create({
+    data: { name, email, password: hashed },
+  });
   return { message: "User registered successfully." };
 };
 
 export const signinUser = async (email: string, password: string) => {
-  const user = users.find((u) => u.email === email);
+  const user = await prisma.user.findUnique({ where: { email } });
   if (!user) throw new Error("Invalid credentials");
   const match = await bcrypt.compare(password, user.password);
   if (!match) throw new Error("Invalid credentials");
