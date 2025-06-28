b032085c9dd4444f8d44496a3251fbed0d42faa4
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-28 11:06:40 +0100

    Add signout controller.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -33,9 +33,30 @@ export const signinHandler = async (req: Request, res: Response) => {
       email: valid.data.email,
       password: valid.data.password,
     });
-    res.json(result);
+
+    const token = result.token;
+        res
+      .cookie("authorization", token, {
+        httpOnly: true, 
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", 
+        maxAge:  7 * 24 * 60 * 60 * 1000, // 7 days.
+      })
+      .status(200)
+      .json({ success:true, message: "Sign in successful!", token: token }); 
   } catch (error) {
     res.status(401).json({ error: error });
   }
+};
+
+export const signoutHandler = async (req: Request, res: Response): Promise<void> => {
+  res
+    .clearCookie("authorization", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    })
+    .status(200)
+    .json({ success:true, message: "Signed out successfully" });
 };
 
