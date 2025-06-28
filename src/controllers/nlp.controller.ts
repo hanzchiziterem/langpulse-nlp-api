ab0f951e35da4f357e7c77d234095a61375bf1f1
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-28 09:11:39 +0100

    Add type checking to nlp controller.

--- src/controllers/nlp.controller.ts
+++ src/controllers/nlp.controller.ts
@@ -1,47 +1,54 @@
-import { Request, Response } from "express";
+import {Request, Response } from "express";
+import prisma from "../lib/prisma";
 import { analyzeText } from "../services/nlp.service";
 import { inputSchema } from "../schemas/input.schema";
-import prisma from "../lib/prisma";
+import { AnalyzeInput } from "../types/nlp";
 
-export const analyzeTextHandler = async (req: Request, res: Response) => {
-  const {text} = req.body;
-  const user = (req as any).user;
+export const analyzeTextHandler = async (
+  req: Request<{}, {}, AnalyzeInput>,
+  res: Response
+) => {
+  const { text } = req.body;
   const validation = inputSchema.safeParse(req.body);
   if (!validation.success) {
     res.status(400).json({ error: validation.error.errors });
     return;
   }
   try {
-    const result = await analyzeText(user.id, text);
+    const result = await analyzeText(req.user!.id, text);
     res.status(201).json({ result });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Error analyzing text" });
   }
 };
 
-export const getAnalysisHistory = async (req: Request, res: Response) => {
-  const user = (req as any).user;
-
+export const getAnalysisHistory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const history = await prisma.analysis.findMany({
-    where: { userId: user.id },
-    orderBy: { createdAt: 'desc' },
+    where: { userId: req.user!.id },
+    orderBy: { createdAt: "desc" },
   });
 
   res.json(history);
-}
+};
 
-// Not tested
-export const downloadAnalysisHistory = async (req: Request, res: Response) => {
-const user = (req as any).user;
-
+export const downloadAnalysisHistory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const data = await prisma.analysis.findMany({
-    where: { userId: user.id },
+    where: { userId: req.user!.id },
     select: { text: true, result: true, createdAt: true },
-    orderBy: { createdAt: 'desc' }
+    orderBy: { createdAt: "desc" },
   });
 
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Content-Disposition', 'attachment; filename=analysis_report.json');
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=analysis_report.json"
+  );
   res.send(JSON.stringify(data, null, 2));
-}
-
+};
