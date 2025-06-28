30e4941dbb2b88c8ca39c5e18fcef9f468fcae01
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-28 06:33:15 +0100

    Add type checking to auth services.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,34 +1,58 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma";
+import { User } from "../generated/prisma";
+
+interface SignUpUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface SignInUser {
+  email: string;
+  password: string;
+}
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export const signupUser = async (
-  name: string,
-  email: string,
-  password: string
-) => {
-  const existing = await prisma.user.findUnique({ where: { email } });
+export const signupUser = async ({
+  name,
+  email,
+  password,
+}: SignUpUser): Promise<{ success: boolean; message: string }> => {
+  const existing: User | null = await prisma.user.findUnique({
+    where: { email },
+  });
+
   if (existing) throw new Error("User already exists");
 
   const hashed = await bcrypt.hash(password, 10);
-  const newUser = await prisma.user.create({
+
+  await prisma.user.create({
     data: { name, email, password: hashed },
   });
-  return { message: "User registered successfully." };
+  return { success: true, message: "User signup is successful." };
 };
 
-export const signinUser = async (email: string, password: string) => {
-  const user = await prisma.user.findUnique({ where: { email } });
+export const signinUser = async ({
+  email,
+  password,
+}: SignInUser): Promise<{
+  success: boolean;
+  message: string;
+  token: string;
+}> => {
+  const user: User | null = await prisma.user.findUnique({ where: { email } });
+  if (user === null) throw new Error("user must not be null.");
   if (!user) throw new Error("Invalid credentials");
-  const match = await bcrypt.compare(password, user.password);
+  const match: boolean = await bcrypt.compare(password, user.password);
+
   if (!match) throw new Error("Invalid credentials");
-  if (!JWT_SECRET) {
-    return;
-  }
+  if (!JWT_SECRET) throw new Error("JWT_SECRET not set");
+
   const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "7d",
   });
-  return { token };
+  return { success: true, message: "User sigin successful", token };
 };
