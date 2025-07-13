1c8718c88a4a57108983a8ec11fe9334f1ae01e6
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-07-14 00:36:22 +0100

    Update and Fix bugs in auth middleware.

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,37 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import prisma from "../client/prisma";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET!;
 
 export const requireAuth = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).json({ error: "Unauthorized" });
+    res
+      .status(401)
+      .json({ success: false, message: "Unauthorized or missing authorization header." });
     return;
   }
-  
   const token = authHeader.split(" ")[1];
-  const blacklisted = await prisma.blacklistedToken.findUnique({
-    where: { token },
-  });
-
-   if (blacklisted) {
-    return res.status(401).json({ message: "Token is revoked. Please sign in again." });
-  }
-  
   try {
-    if (!JWT_SECRET) {
-      return;
-    }
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_TOKEN_SECRET);
     (req as any).user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ error: "Invalid token" });
+    res.status(401).json({ success: false, error: "Invalid or expired access token." });
   }
 };
