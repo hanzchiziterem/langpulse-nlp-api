3e1e2c78ba86b7ff55023344173caf5bf2831baa
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-07-21 05:25:44 +0100

    feat: Update app file.

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,18 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import OpenAI from "openai";
 
+import { securityContextMiddleware } from './middlewares/security.middleware';
+import { ensureUploadsDirExists } from './utils/cloudinary/fileHelper';
+
 //Routes
 import v1Routes from './routes/v1';
 
-
 //Configutations
 dotenv.config();
+ensureUploadsDirExists();
 const app = express();
+app.set('trust proxy', 1);
+
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -22,6 +27,8 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser());
+
+app.use(securityContextMiddleware);
 
 //Routes
 app.use('/api/v1', v1Routes);
