f04b6c197c57c78e00760de9404d76c57101c559
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-07-23 09:48:26 +0100

    feat(test): Update mock for prisma.

--- src/__tests__/__mocks__/prisma.ts
+++ src/__tests__/__mocks__/prisma.ts
@@ -1,8 +1,11 @@
-export default {
+const mockPrisma = {
   user: {
     create: jest.fn(),
     findUnique: jest.fn(),
     findFirst: jest.fn(),
     update: jest.fn(),
   },
+  $disconnect: jest.fn(),
 };
+
+export default mockPrisma;
