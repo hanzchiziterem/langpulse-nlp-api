9e4472712e5ff973126ec59db6e5e545d5853779
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-07-24 18:42:14 +0100

    Change jest config file from js to ts.

---
+++ jest.config.ts
@@ -0,0 +1,38 @@
+import { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: ".",
+  testRegex: ".*\\.(spec|test)\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/index.ts",
+    "!src/**/*.interface.ts",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  moduleDirectories: ["node_modules", "src"],
+  clearMocks: true,
+  setupFiles: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+export default config;
