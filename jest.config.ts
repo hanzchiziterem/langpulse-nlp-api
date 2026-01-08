fa33e5ae4e997198b079a1d04a512f7b015f30c2
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2026-01-08 08:17:51 +0100

    feat(fix-to-test):[SCRUM-SPRINT-1/SCURM-5] Updated jest config and tsconfig file.

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   rootDir: "./",
   testRegex: ".*\\.(spec|test)\\.ts$",
   transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
+    "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.json" }],
   },
   moduleFileExtensions: ["ts", "js", "json", "node"],
   collectCoverage: true,
@@ -18,18 +18,13 @@ const config: Config = {
     "!src/**/*.interface.ts",
   ],
   moduleNameMapper: {
-    "^@/(.*)$": ["<rootDir>/src/$1"],
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 
   moduleDirectories: ["node_modules", "src"],
   clearMocks: true,
   setupFiles: ["<rootDir>/jest.setup.ts"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
-    testMatch: ["**/__tests__/**/*.test.ts"],
     coverageThreshold: {
       global: {
         branches: 80,
@@ -38,7 +33,6 @@ const config: Config = {
         statements: 80,
       },
     },
-  },
-};
+  };
 
 export default config;
